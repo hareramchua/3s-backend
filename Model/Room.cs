eb7cb68aedb99934f8804eb6c37d0ea22de039e9
RusselHarveyM <russelharvey.mercado@cit.edu>
2024-03-15 14:31:57 +0800

    fix room model

--- Model/Room.cs
+++ Model/Room.cs
@@ -8,7 +8,7 @@ namespace _5s.Model
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        public int BuildingId { get; set; }
+        public string BuildingId { get; set; }
         public string? RoomNumber { get; set; }
         public byte[]? Image { get; set; }
         public string Status { get; set; }
